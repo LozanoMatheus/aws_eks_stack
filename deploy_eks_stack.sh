398e3cd7d15b4ace1667862d44c66293ba677ac0
Matheus Lozano <lozanomatheus@gmail.com>
2019-09-23 00:45:46 +0200

    Moving all vars/functions to a shell include script Add setup and app_deploy choice

--- deploy_eks_stack.sh
+++ deploy_eks_stack.sh
@@ -1,61 +1,57 @@
 #!/usr/bin/env bash
 
-declare -xr PROJECT_BASEDIR="$(dirname "${BASH_SOURCE[0]}" | pwd)"
-declare -xr CONFIG_DIR="${PROJECT_BASEDIR}/config_files"
-declare -r  TERRAFORM_VERSION="${TERRAFORM_VERSION:-'0.12.4'}"
-declare -r  AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE:-~/.aws/credentials}
-declare -r  LOG_FILE="/tmp/${0//.sh/}_$(date +'%Y%m%d').log"
-declare -rl KERNEL_NAME="$(uname -s)"
-declare -xr AWS_REGION="${AWS_REGION:-"eu-west-2"}"
-declare -xr CLUSTER_NAME="${CLUSTER_NAME:-"mlozano-eks-cluster"}"
-declare -xr TF_VAR_my_public_ip="${MY_PUBLIC_IP:-"$(curl -s ifconfig.co)"}"
-declare -xr TF_VAR_ssh_key_name="${SSH_KEY_NAME:-"ssh_key_eks_workers"}"
-declare -xr TF_VAR_ssh_key_path="${SSH_KEY_PATH:-"${PROJECT_BASEDIR}/keys"}"
-
-function log_msg() {
-  echo "$(date +'%Y-%m-%d %T')" $@ | tee -a "${LOG_FILE}"
-}
+set -o allexport
 
 for i in bash_scripts/*.shinc ; do
   . "${i}"
 done
 
 log_msg "Checking for dependencies"
-check_prereqs
+pre_deploy_check
 
 if [[ -z "${1}" ]]; then
-  declare -r TERRAFORM_QUESTION="What action you want to execute?
-  1) Deploy
-  2) Destroy
+  declare -r MAIN_QUESTION="What action you want to execute?
+  1) Deploy     3) Setup
+  2) Destroy    4) App_deploy
   Default: Deploy
   -> "
-
-  read -ep "${TERRAFORM_QUESTION}" terraform_action
-  declare -lr TERRAFORM_ACTION="${terraform_action:-"Deploy"}"
+  read -ep "${MAIN_QUESTION}" main_action
+  declare -lr MAIN_ACTION="${main_action:-"Deploy"}"
 else
-  declare -lr TERRAFORM_ACTION="${1:-"Deploy"}"
+  declare -lr MAIN_ACTION="${1:-"Deploy"}"
 fi
 
-case "${TERRAFORM_ACTION}" in
+case "${MAIN_ACTION}" in
   1|deploy)
     log_msg "Starting to deploy the AWS EKS stack"
     cd terraform_automation/
     terraform_deploy
-    setup_kubectl
-    terraform_write_output
-    setup_cluster
-    setup_helm
     exit
     ;;
   2|destroy)
     log_msg "Starting to destroy the AWS EKS stack"
     cd terraform_automation/
-    delete_apps
     terraform_destroy
+    [[ $? -eq 0 ]] && rm -rf *tfstate* .terraform/
+    exit
+    ;;
+  3|setup)
+    setup_environment
+    exit
+    ;;
+  4|app_deploy)
+    declare -r SETUP_QUESTION="Do you want setup the cluster? y/n
+    Default: n
+    -> "
+    read -ep "${SETUP_QUESTION}" setup_action
+    declare -lr SETUP_ACTION="${setup_action:-"n"}"
+    [[ "${SETUP_ACTION// /}" == 'y' ]] && setup_environment
+    declare -ra APPS_TO_DEPLOY="${BASH_ARGV[@]/4}"
+    deploy_apps ${APPS_TO_DEPLOY[@]}
     exit
     ;;
   *)
-    log_msg "The action \"${TERRAFORM_ACTION}\" is not known"
+    log_msg "The action \"${MAIN_ACTION}\" is not known"
     exit
     ;;
 esac
