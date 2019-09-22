b4530f0d91e53c0ac360ef467b13b7040943f2c2
Matheus Lozano <lozanomatheus@gmail.com>
2019-09-23 00:36:35 +0200

    Add iam policy to helm autoscaler (Allow manage the ASGs).

--- terraform_automation/worker_node.tf
+++ terraform_automation/worker_node.tf
@@ -3,6 +3,30 @@ resource "aws_key_pair" "ssh_key" {
   public_key = "${file("${var.ssh_key_path}/${var.ssh_key_name}.pub")}"
 }
 
+resource "aws_iam_policy" "helm-autoscaler" {
+  name        = "helm_autoscaler"
+  path        = "/"
+  description = "Policy for Kubernetes Helm Autoscaler"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_role" "demo-node" {
   name = "${var.cluster_name}-node"
 
@@ -20,6 +44,11 @@ resource "aws_iam_role" "demo-node" {
   ]
 }
 POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "helm-autoscaler" {
+  role       = "${aws_iam_role.demo-node.name}"
+  policy_arn = "${aws_iam_policy.helm-autoscaler.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
@@ -119,13 +148,13 @@ USERDATA
 
 resource "aws_launch_configuration" "demo" {
   associate_public_ip_address = true
-  iam_instance_profile        = "${aws_iam_instance_profile.demo-node.name}"
-  image_id                    = "${data.aws_ami.eks-worker.id}"
-  instance_type               = "${var.instance_type}"
-  name_prefix                 = "${var.cluster_name}"
-  security_groups             = ["${aws_security_group.demo-node.id}"]
-  user_data_base64            = "${base64encode(local.demo-node-userdata)}"
-  key_name                    = "${var.ssh_key_name}"
+  iam_instance_profile = "${aws_iam_instance_profile.demo-node.name}"
+  image_id = "${data.aws_ami.eks-worker.id}"
+  instance_type = "${var.instance_type}"
+  name_prefix = "${var.cluster_name}"
+  security_groups = ["${aws_security_group.demo-node.id}"]
+  user_data_base64 = "${base64encode(local.demo-node-userdata)}"
+  key_name = "${var.ssh_key_name}"
 
   lifecycle {
     create_before_destroy = true
@@ -133,22 +162,22 @@ resource "aws_launch_configuration" "demo" {
 }
 
 resource "aws_autoscaling_group" "demo" {
-  desired_capacity     = 2
+  desired_capacity = 2
   launch_configuration = "${aws_launch_configuration.demo.id}"
-  max_size             = 2
-  min_size             = 1
-  name                 = "${var.cluster_name}"
-  vpc_zone_identifier  = "${aws_subnet.demo.*.id}"
+  max_size = 2
+  min_size = 1
+  name = "${var.cluster_name}"
+  vpc_zone_identifier = "${aws_subnet.demo.*.id}"
 
   tag {
-    key                 = "Name"
-    value               = "${var.cluster_name}"
+    key = "Name"
+    value = "${var.cluster_name}"
     propagate_at_launch = true
   }
 
   tag {
-    key                 = "kubernetes.io/cluster/${var.cluster_name}"
-    value               = "owned"
+    key = "kubernetes.io/cluster/${var.cluster_name}"
+    value = "owned"
     propagate_at_launch = true
   }
 }
